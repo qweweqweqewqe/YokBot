3ecfc4c90fe4b45a28d9f28366611bc28fb99328
sjh <sjh@DESKTOP-NSKRR5U>
2024-01-19 00:27:35 -0500

    Add project files.

---
+++ YokBot/Program.cs
@@ -0,0 +1,74 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YokBot.config;
+
+namespace YokBot
+{
+    public sealed class Program
+    {
+        static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
+        {
+            var tokenAccesser = new TokenAccess();
+            await tokenAccesser.ReadJSON();
+
+            var discConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = tokenAccesser.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discConfig);
+
+            Client.UseInteractivity(new InteractivityConfiguration() { Timeout = TimeSpan.FromMinutes(2) });
+
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+        {
+            Client.MessageAcknowledged += MessageHandler;
+            Commands = Client.UseCommandsNext(new CommandsNextConfiguration());
+            return Task.CompletedTask;
+        }
+        static Dictionary<string, int> wordCounts = new Dictionary<string, int>()
+        {
+            {"shite", 0},
+            {"sht", 0},
+            {"fk", 0},
+        };
+        private static async Task MessageHandler(DiscordClient client, MessageAcknowledgeEventArgs args)
+        {
+            string messageContent = args.Message.Content.ToLower();
+
+            if (args.Message.Content.ToLower() == "~swearcount")
+            {
+                int totalCount = wordCounts.Values.Sum();
+                await args.Channel.SendMessageAsync($"You have sworn a total of {totalCount} times.");
+            }
+
+                foreach (var word in wordCounts.Keys)
+            {
+                if (messageContent.Contains(word))
+                {
+                    wordCounts[word]++;
+                    await args.Channel.SendMessageAsync($"{args.Message.Author.Mention} has said {word}. They have said it {wordCounts[word]} times!");
+                    break; 
+                }
+            }
+        }
+    }
+}
---
+++ YokBot/config/TokenAccess.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YokBot.config
+{
+    internal class TokenAccess
+    {
+        public string token { get; set; }
+        public string call { get; set; }
+
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                Structure data = JsonConvert.DeserializeObject<Structure>(json);
+
+                this.token = data.token;
+                this.call = data.call;
+            }
+        }
+
+    }
+
+    internal sealed class Structure{
+        public string token {  set; get; }  
+        public string call { set; get; }
+    
+    }
+
+}
